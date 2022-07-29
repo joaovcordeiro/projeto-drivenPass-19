cd40d068c65f574548f8e55fe95e74f9b8658721
Vinicius <vinigugelmin@hotmail.com>
2022-07-29 16:39:56 -0300

    Preparando revisão de código

--- prisma/migrations/20220715182024_remove_answer_from_sections_table/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `answer` on the `section` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "section" DROP COLUMN "answer";
--- src/schemas/secureNoteSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-import { SecureNoteCreateData } from "../services/secureNoteService.js";
-
-export const secureNoteSchema = Joi.object<SecureNoteCreateData>({
-    noteTitle: Joi.string().max(50).required(),
-    annotation: Joi.string().max(1000).required()
-});
